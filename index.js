c58b983e8b11a123cfeddf22bc94e3fa0247f2af
shahbaz12243 <shahbazz12243@gmail.com>
2025-04-22 11:56:42 +0530

    done

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const { error } = require("console");
 const express = require("express")
 const app = express()
 const port = 3000;
  
 app.use(express.json())
 
-const users= [
+const userDetails= [
     {username:'alice',age:25,email:"alice@example.com"},
     { username:'bob',age:30,email:"bob@example.com"},
     {username:'charlie',age:28,email:"charlie@example.com"}
 ]
-
-
-app.post("/User",(req,res)=>{
+app.get("/Users",async (req,res)=>{
     const {username,age,email}= req.body;
     if(!username||!age||!email)
         return res.status(400).json({error:"User parameter cannot be empty"})
-    if(!users)
+    if(!userDetails)
         return res.status(400).json({error:"User not found"})
 
-    res.status(201).json({message:"User found","data":{users}})
+
+   const User = await userDetails.find()
+   res.status(201).json({message:"User found",User,"data":{userDetails}})
 })
+
 
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`)
